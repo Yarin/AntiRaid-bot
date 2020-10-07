c09fdc308a89e4391705dbd8e703e20cc5da95a0
xYarin <yarinm0206@gmail.com>
2020-10-07 19:33:20 +0300

    added newMembers command. command to output how many new users are in the guild

--- cogs/notify.py
+++ cogs/notify.py
@@ -7,35 +7,50 @@ class Notifier(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    def isNew(self, member):
+        age = member.created_at
+        now = datetime.now()
+        now -= timedelta(days=7)
+        if age >= now:
+            return True
+        return False
+
     @commands.Cog.listener()
     async def on_member_join(self, member):
         print(f"member joined {member.guild.name}")
         server_id = member.guild.id
         config = Config('config', str(server_id))
-        if config.isCheckingAge():
-            age = member.created_at
-            now = datetime.now()
-            now -= timedelta(days=7)
-            if age >= now:
-                days = datetime.now() - age
-                print(f"[DETECTED] New User in {member.guild.name}")
-                staff_chat = discord.utils.get(member.guild.text_channels, id=config.getNotifyChat())
-                embed=discord.Embed(title="New User Notify", description=f"<@{member.id}> has joined", color=0xe11414)
-                embed.add_field(name="Creation date:", value=f"{days.days if days.days > 0 else 0} days ago, {days.seconds // 3600} hours ago, {days.seconds // 60 % 60} minutes ago", inline=True)
-                embed.set_author(name="AntiRaider Notifier", icon_url=self.bot.user.avatar_url)
-                embed.set_thumbnail(url=member.avatar_url)
-                await staff_chat.send(embed=embed)
-                role = config.getRoleNotifierId()
-                if role != 0:
-                    if type(role) == list:
-                        msg = ""
-                        for r in role:
-                            msg += f" <@&{r}> "
-                        await staff_chat.send(msg)
-                    else:
-                        await staff_chat.send(f"<@&{role}>")
+        if config.isCheckingAge() and self.isNew(member):  
+            days = datetime.now() - member.created_at
+            print(f"[DETECTED] New User in {member.guild.name}")
+            staff_chat = discord.utils.get(member.guild.text_channels, id=config.getNotifyChat())
+            embed=discord.Embed(title="New User Notify", description=f"<@{member.id}> has joined", color=0xe11414)
+            embed.add_field(name="Creation date:", value=f"{days.days if days.days > 0 else 0} days ago, {days.seconds // 3600} hours ago, {days.seconds // 60 % 60} minutes ago", inline=True)
+            embed.set_author(name="AntiRaider Notifier", icon_url=self.bot.user.avatar_url)
+            embed.set_thumbnail(url=member.avatar_url)
+            await staff_chat.send(embed=embed)
+            role = config.getRoleNotifierId()
+            if role != 0:
+                if type(role) == list:
+                    msg = ""
+                    for r in role:
+                        msg += f" <@&{r}> "
+                    await staff_chat.send(msg)
                 else:
-                    await staff_chat.send(member.guild.default_role)
+                    await staff_chat.send(f"<@&{role}>")
+            else:
+                await staff_chat.send(member.guild.default_role)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def newMembers(self, ctx):
+        members = ctx.guild.members
+        newMembers = 0
+        for member in members:
+            if self.isNew(member):
+                newMembers += 1
+        await ctx.send(f"{newMembers} New Members in {ctx.guild.name}")
+
 
 
 def setup(bot):
